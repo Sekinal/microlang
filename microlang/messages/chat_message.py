a45ad70202b69097a8132afa55866b0a7377270f
Thermostatic <muxxeroerty@gmail.com>
2024-01-28 16:09:44 -0600

    Improved the definition for ChatMessage. TODO: Fix docstrings.

--- microlang/messages/chat_message.py
+++ microlang/messages/chat_message.py
@@ -1,28 +1,28 @@
 from pydantic import BaseModel
 import tiktoken
 class ChatMessage(BaseModel):
-
-    role: str
     value: str
-    
-    def __init__(self, role: str, value: str) -> None:
+    sender: str
+    encoding: str = "cl100k_base"
+    tokens: int = 0
+
+    def __init__(self, value: str, sender: str, encoding: str = encoding) -> None:
         """Define a new chat message.
 
         Args:
-            role (str): The role of the sender.
-            value (str): The contained string in the message.
+            value (str): The role of the sender.
+            sender (str): The contained string in the message.
+            encoding_val (str, optional): The encoding to be used for the message. Defaults to "cl100k_base".
         """
-        super().__init__(role = role, value = value)
+        super().__init__(value=value, sender=sender, encoding=encoding)
+        self.tokens = self.get_tokens()
         
-    def get_tokens(self, encoding: str = "cl100k_base") -> int:
+    def get_tokens(self) -> int:
         """A method to calculate the amount of tokens contained in the parent ChatMessage.
-
-        Args:
-            encoding (str, optional): The tiktoken supported encoding of the used model. Defaults to "cl100k_base".
-
+        
         Returns:
             int: The amount of tokens contained in the parent ChatMessage.
         """
-        encoder = tiktoken.get_encoding(encoding)
+        encoder = tiktoken.get_encoding(self.encoding)
         
         return len(encoder.encode(self.value))
