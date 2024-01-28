6cdfba17abfc788cf269102b56e88cace8e5753c
Thermostatic <muxxeroerty@gmail.com>
2024-01-28 14:37:10 -0600

    Added tiktoken test and improved previous tests.

--- tests/messages/test_chatmessages.py
+++ tests/messages/test_chatmessages.py
@@ -1,12 +1,19 @@
-import pytest
+import tiktoken
 import microlang
 
-def test_ChatMessage():
+encoder = tiktoken.get_encoding("cl100k_base")
+def test_ChatMessage_1():
     message = microlang.messages.ChatMessage(role = "gpt", value = "hey")
     
-    assert message.value == "hey"
+    assert message.role == "gpt" and message.value == "hey"
 
-def test_tokens():
-    message = microlang.messages.ChatMessage(role = "gpt", value = "hey")
+def test_ChatMessage_2():
+    message = microlang.messages.ChatMessage(role = "user", value = "hey")
     
-    assert message.value == "hey"
+    assert message.role == "user" and message.value == "hey"
+    
+def test_tokens():
+    msg_string = "Hey, how are you today?"
+    message = microlang.messages.ChatMessage(role = "user", value = msg_string)
+    
+    assert len(encoder.encode(msg_string)) == message.get_tokens()
