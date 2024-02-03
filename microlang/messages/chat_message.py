d934080b2de7c8874d9d94f10d6f0322c625af57
Thermostatic <muxxeroerty@gmail.com>
2024-02-03 01:24:11 -0600

    Converted tokens from a class attribute to a callable method.

--- microlang/messages/chat_message.py
+++ microlang/messages/chat_message.py
@@ -3,26 +3,24 @@ import tiktoken
 class ChatMessage(BaseModel):
     value: str
     sender: str
-    encoding: str = "cl100k_base"
     tokens: int = 0
 
-    def __init__(self, value: str, sender: str, encoding: str = encoding) -> None:
+    def __init__(self, value: str, sender: str) -> None:
         """Define a new chat message.
 
         Args:
             value (str): The role of the sender.
             sender (str): The contained string in the message.
-            encoding_val (str, optional): The encoding to be used for the message. Defaults to "cl100k_base".
         """
-        super().__init__(value=value, sender=sender, encoding=encoding)
-        self.tokens = self.get_tokens()
+        super().__init__(value=value, sender=sender)
         
-    def get_tokens(self) -> int:
+    def tokens(self, encoding:str = "cl100k_base") -> int:
         """A method to calculate the amount of tokens contained in the parent ChatMessage.
         
         Returns:
             int: The amount of tokens contained in the parent ChatMessage.
+            encoding (str, optional): The encoding to be used for the message. Defaults to "cl100k_base".
         """
-        encoder = tiktoken.get_encoding(self.encoding)
+        encoder = tiktoken.get_encoding(encoding)
         
         return len(encoder.encode(self.value))
