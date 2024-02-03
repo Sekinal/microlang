1e687b2e1a00e723b8022356ebdd0c97982a947e
Thermostatic <muxxeroerty@gmail.com>
2024-02-03 00:54:10 -0600

    Modified docstrings. Added token functionality. Removed string msg del.

--- microlang/memory/conversation_memory.py
+++ microlang/memory/conversation_memory.py
@@ -7,57 +7,33 @@ from ..messages import ChatMessage
 
 class ConversationMemory(BaseModel):
     messages: List[ChatMessage] = []
+    tokens: int = 0
 
-    def __init__(self, messages: List[ChatMessage] = messages) -> None:
-        super().__init__(messages=messages)
+    def __init__(self, messages: List[ChatMessage] = messages, tokens: int = tokens) -> None:
+        super().__init__(messages=messages, tokens = tokens)
 
     def add_message(self, message: ChatMessage) -> None:
-        """
-        Adds a ChatMessage to the list of messages.
+        """Adds a ChatMessage to the list of messages.
 
         Args:
-            self: The instance of the ConversationMemory class.
-            message (ChatMessage): The ChatMessage object to be added.
-
-        Returns:
-            None
-
-        Examples:
-        >>> memory = ConversationMemory()
-        >>> message = ChatMessage(value = "Hello", sender="Alice")
-        >>> memory.add_message(message)
+            message (ChatMessage): The ChatMessage to be added.
         """
 
         self.messages.append(message)
+        self.tokens += message.tokens
 
-    def delete_message(self, to_remove: int or str) -> None:
-        """
-        Deletes a message from the conversation memory.
+    def delete_message(self, to_remove: int) -> None:
+        """Deletes a ChatMessage from the list of messages.
 
         Args:
-            to_remove (int or str): The index or content of the message to be removed.
-            If a string is given, it deletes the first encounter of the string from right to left.
-
-        Returns:
-            None
-
-        Raises:
-            None
-
-        Examples:
-            By using index,
-            >>> memory = ConversationMemory()
-            >>> message = ChatMessage(value = "Hello", sender="Alice")
-            >>> memory.add_message(message)
-            >>> memory.delete_message(0)
-            By using string,
-            >>> memory.delete_message("Hello")
+            to_remove (int): The index of the ChatMessage to remove.
         """
-
-        if isinstance(to_remove, int):
-            del self.messages[to_remove]
-        elif isinstance(to_remove, str):
-            self.messages = self.messages[::-1].remove(to_remove)
+        
+        self.tokens -= self.messages[to_remove].tokens
+        del self.messages[to_remove]
 
     def clear_conversation(self) -> None:
+        """Clears the conversation memory from all ChatMessages.
+        """
+        
         self.messages.clear()
